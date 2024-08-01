574b5d05c34fea63f7c26bfe29b77278b8e14c19
Nathan Luz <nathanluz@Nathans-MacBook-Pro.local>
2024-08-01 20:09:58 -0300

    adding docker files

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("get to status page should return 200", () => {
+test("get to status page OK", () => {
   fetch("http://localhost:3000/api/v1/status")
     .then((response) => {
       expect(response.status).toBe(200);
