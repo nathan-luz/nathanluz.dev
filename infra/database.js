5db0ee5d7969cde9d87a07a2aec9738e2b2918fa
Nathan Luz <nathan_luz@icloud.com>
2024-08-08 18:57:48 -0300

    compose file with env var

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from 'pg'
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+    database: "postgres",
   });
   await client.connect();
   const result = await client.query(queryObject);
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("get to status page OK", () => {
+test("get to status page", () => {
   fetch("http://localhost:3000/api/v1/status")
     .then((response) => {
       expect(response.status).toBe(200);
